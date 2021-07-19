efb1de2b240cba52c2b81df60292f5ba2b7740f8
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-07-19 19:54:28 +0100

    Updating logistic regression

--- logistic-regression.py
+++ logistic-regression.py
@@ -1,2 +1,21 @@
+#%%
+from numpy import load
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
+import pandas as pd
+
+X, y = load_breast_cancer(return_X_y=True)
+
+X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.8)
+X_train, y_train, X_val, y_val = train_test_split(X, y, test_size=0.8)
+
+X.shape
+
+df = pd.DataFrame(X)
+data = load_breast_cancer()
+data.keys()
+column_names = load_breast_cancer()['feature_names']
+df.columns = column_names
+
+print(df)
